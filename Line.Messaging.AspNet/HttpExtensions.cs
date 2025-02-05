e9ab1b63bb4fc813dd584bbdad8ba3e84588e539
Ruxo Zheng <rz@just.sent.as>
2025-02-05 11:41:04 +0700

    Add utility class for setting up HttpClient

---
+++ Line.Messaging.AspNet/HttpExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http.Headers;
+
+namespace Line.Messaging.AspNet;
+
+public static class HttpExtensions
+{
+    public static HttpClient SetupForLineMessageClient(this HttpClient http, string apiKey) {
+        http.BaseAddress = new Uri(LineMessagingClient.OfficialUri);
+        http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+        return http;
+    }
+}
